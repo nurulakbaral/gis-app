94003ce0d8b94f6e90490fcd2a32009e91255ad7
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-10-05 10:00:35 +0700

    Chore: Setup Again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@mantine/core/styles.css'
-import '~/styles/globals.css'
+import '~/src/assets/globals.css'
 import type { AppProps } from 'next/app'
 import { createTheme, MantineProvider } from '@mantine/core'
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <Button>Hello World!</Button>
+      <h1 className='text-green-600'>Hello</h1>
     </div>
   )
 }
