b99a0ec44b19585c98baab2459bb1d61d7b44667
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-10-06 09:50:05 +0700

    Feat: Render List and Layer Group

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,8 @@ const Fixtures = {
       position: [-6.194883674005233, 106.8178317822333],
       imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipMIbJMaN3PiGD_CXQS80T5FbAkrbD2GN7oEQxtO=w408-h306-k-no',
       description:
-        "Jakarta, Indonesia's massive capital, sits on the northwest coast of the island of Java. A historic mix of cultures – Javanese, Malay, Chinese, Arab, Indian and European – has influenced its architecture, language and cuisine.",
+        "Jakarta, Indonesia's massive capital, sits on the northwest coast of the province of Java. A historic mix of cultures – Javanese, Malay, Chinese, Arab, Indian and European – has influenced its architecture, language and cuisine.",
+      type: 'province',
     },
     {
       id: 'bandung',
@@ -21,6 +22,7 @@ const Fixtures = {
       imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipPWrXwQSyPZlSWG5PN02hX6sVkzrF4n6AYjaINq=w426-h240-k-no',
       description:
         'Bandung, capital of Indonesia’s West Java province, is a large city set amid volcanoes and tea plantations.',
+      type: 'province',
     },
     {
       id: 'bali',
@@ -29,7 +31,8 @@ const Fixtures = {
       position: [-8.404869639881086, 115.18724233574186],
       imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipNfIq8CczQOuMOO97vrvtUFeVEKD-MmiguaEykm=w408-h272-k-no',
       description:
-        'Bali is a province of Indonesia and the westernmost of the Lesser Sunda Islands. East of Java and west of Lombok, the province includes the island of Bali and a few smaller offshore islands, notably Nusa Penida, Nusa Lembongan, and Nusa Ceningan to the southeast.',
+        'Bali is a province of Indonesia and the westernmost of the Lesser Sunda provinces. East of Java and west of Lombok, the province includes the province of Bali and a few smaller offshore provinces, notably Nusa Penida, Nusa Lembongan, and Nusa Ceningan to the southeast.',
+      type: 'province',
     },
     {
       id: 'east-kalimantan',
@@ -38,7 +41,8 @@ const Fixtures = {
       position: [0.6266134143493987, 116.53067177408195],
       imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipMDHy-NNLLvKKWj4njGfCsy1EreADMeukEx_gU8=w519-h240-k-no',
       description:
-        'East Kalimantan is an Indonesian province in the east of the island of Borneo. It’s known for its indigenous Dayak culture and rainforest areas like Kutai National Park, home to orangutans.',
+        'East Kalimantan is an Indonesian province in the east of the province of Borneo. It’s known for its indigenous Dayak culture and rainforest areas like Kutai National Park, home to orangutans.',
+      type: 'province',
     },
     {
       id: 'monas',
@@ -48,6 +52,7 @@ const Fixtures = {
       imageUrl: 'https://lh5.googleusercontent.com/p/AF1QipN0gblPqvGKJ5gmA4HfrW72Kzjbqz-R6tYUMAif=w408-h479-k-no',
       description:
         'The National Monument is a 132 m obelisk in the centre of Merdeka Square, Central Jakarta. It is the national monument of the Republic of Indonesia, built to commemorate the struggle for Indonesian independence.',
+      type: 'location',
     },
     {
       id: 'ikn',
@@ -56,7 +61,8 @@ const Fixtures = {
       position: [-0.960730614510819, 116.69818381299989],
       imageUrl: 'https://maps.gstatic.com/tactile/pane/default_geocode-2x.png',
       description:
-        'Nusantara (Indonesian pronunciation: [nusanˈtara]), officially the Capital City of Nusantara (Indonesian: Ibu Kota Nusantara, abbreviated IKN), is the future capital of Indonesia, located between Kutai Kartanegara Regency and Penajam North Paser Regency, East Kalimantan, on the island of Borneo.',
+        'Nusantara (Indonesian pronunciation: [nusanˈtara]), officially the Capital City of Nusantara (Indonesian: Ibu Kota Nusantara, abbreviated IKN), is the future capital of Indonesia, located between Kutai Kartanegara Regency and Penajam North Paser Regency, East Kalimantan, on the province of Borneo.',
+      type: 'location',
     },
   ] as Array<TMapLayer>,
 }
@@ -68,7 +74,7 @@ const Maps = dynamic(() => import('~/src/features/location/Maps'), {
 
 export default function Home() {
   return (
-    <section className='px-10 py-8'>
+    <section className='pt-6'>
       <Head>
         <title>GIS App</title>
       </Head>
--- src/features/location/Maps.tsx
+++ src/features/location/Maps.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import { Icon, LatLngExpression } from 'leaflet'
-import { MapContainer, TileLayer, MapContainerProps, Popup, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, MapContainerProps, LayerGroup, Marker, LayersControl } from 'react-leaflet'
+import { useDisclosure } from '@mantine/hooks'
+import { Drawer, Button, Box } from '@mantine/core'
+import Image from 'next/image'
 
 const IconPin = new Icon({
   iconUrl: '/icons/icon-pin.png',
@@ -24,6 +27,7 @@ export type TMapLayer = {
   position: LatLngExpression
   imageUrl: string
   description: string
+  type: 'province' | 'location'
 }
 
 export interface TMapsProps extends MapContainerProps {
@@ -31,6 +35,8 @@ export interface TMapsProps extends MapContainerProps {
 }
 
 export default function Maps({ layerList, ...props }: TMapsProps) {
+  const [currentLayer, setCurrentLayer] = React.useState<null | TMapLayer>(null)
+  const [opened, { open, close }] = useDisclosure(false)
   const Icons = {
     point: IconPin,
     monas: IconMonas,
@@ -38,26 +44,82 @@ export default function Maps({ layerList, ...props }: TMapsProps) {
   }
 
   return (
-    <MapContainer
-      className='h-[640px] w-full rounded-lg'
-      center={[-2.5489, 118.0149]}
-      zoom={5}
-      scrollWheelZoom={true}
-      {...props}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
+    <React.Fragment>
+      <Drawer.Root className='z-[9999999999]' opened={opened} onClose={close}>
+        <Drawer.Content className='z-[9999999999]'>
+          <Box className='relative'>
+            <Image
+              className='w-full'
+              src={currentLayer?.imageUrl || ''}
+              alt={currentLayer?.name || ''}
+              width={200}
+              height={200}
+            />
+            <Drawer.CloseButton className='absolute top-2 right-2 bg-white rounded-full' />
+          </Box>
 
-      {layerList.map(({ id, name, iconName, position, imageUrl, description }) => (
-        <Marker key={id} icon={Icons[iconName]} position={position}>
-          <Popup>
-            <p>{name}</p>
-            <p>{description}</p>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+          <Drawer.Header>
+            <Drawer.Title className='text-3xl'>{currentLayer?.name}</Drawer.Title>
+          </Drawer.Header>
+
+          <Drawer.Body>{currentLayer?.description}</Drawer.Body>
+        </Drawer.Content>
+      </Drawer.Root>
+
+      <MapContainer
+        className='h-[calc(100vh-6rem)] w-full'
+        center={[-2.5489, 118.0149]}
+        zoom={5}
+        scrollWheelZoom={true}
+        {...props}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+
+        <LayersControl position='topright'>
+          <LayersControl.Overlay name='Provinces Layer'>
+            <LayerGroup>
+              {layerList
+                .filter(({ type }) => type === 'province')
+                .map(({ id, name, iconName, position, imageUrl, description, type }) => (
+                  <Marker
+                    key={id}
+                    icon={Icons[iconName]}
+                    position={position}
+                    eventHandlers={{
+                      click: () => {
+                        open()
+                        setCurrentLayer({ id, name, iconName, position, imageUrl, description, type })
+                      },
+                    }}
+                  />
+                ))}
+            </LayerGroup>
+          </LayersControl.Overlay>
+
+          <LayersControl.Overlay name='Locations Layer'>
+            <LayerGroup>
+              {layerList
+                .filter(({ type }) => type === 'location')
+                .map(({ id, name, iconName, position, imageUrl, description, type }) => (
+                  <Marker
+                    key={id}
+                    icon={Icons[iconName]}
+                    position={position}
+                    eventHandlers={{
+                      click: () => {
+                        open()
+                        setCurrentLayer({ id, name, iconName, position, imageUrl, description, type })
+                      },
+                    }}
+                  />
+                ))}
+            </LayerGroup>
+          </LayersControl.Overlay>
+        </LayersControl>
+      </MapContainer>
+    </React.Fragment>
   )
 }
